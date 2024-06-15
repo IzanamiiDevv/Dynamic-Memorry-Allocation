6220ed521fabd2c5fea4841329c826ff50257e2b
IzanamiiDevv <rafaeloli596@gmail.com>
2024-06-15 21:49:57 +0800

    ReAllocate

---
+++ ReAllocate.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int* array = malloc(sizeof(int) * 3);
+
+    array[0] = 10;
+    array[1] = 10;
+    array[2] = 20;
+
+    array = realloc(array ,sizeof(int) * 2);
+
+    printf("%i", array[1]);  //10
+    printf("%i", array[2]);  //outside the bond
+    free(array);
+
+    return 0;
+}
